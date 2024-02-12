f5e6917d8aa23657a66a003259e0adb8e3f3e76e
Sivakumar R <108018740+sivakumar-dr@users.noreply.github.com>
2024-02-12 12:21:21 +0000

    sed test done for sql date changes

--- use_case/sed-test.sh
+++ use_case/sed-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APP_HOME="/workspaces/learning-bash-scripting-3212393/use_case"
+APP_HOME="/workspaces/Linux-learning-bash-scripting/use_case"
 SQL_FILE="sample.sql"
 START_DATE=${1}
 END_DATE=${2}
@@ -15,8 +15,7 @@ LOCAL_START_MONTH=$START_MONTH
 if [ $START_MONTH == $END_MONTH ]
 then
     VAR="'$START_DATE' and '$END_DATE'"
-    sed -i -e "s/\(date_key between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
-    sed -i -e "s/\(transaction_date_key between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
+    sed -i -e "s/\(between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
 else
     while [ $LOCAL_START_MONTH != $END_MONTH ]
     do
@@ -24,14 +23,11 @@ else
         THE_END_DATE=$(date -d "$(date -d "$THE_START_DATE +1 month" +%Y-%m-01) -1sec" +%F)
         
         VAR="'$THE_START_DATE' and '$THE_END_DATE'"
-        sed -i -e "s/\(date_key between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
-        sed -i -e "s/\(transaction_date_key between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
-        
+        sed -i -e "s/\(between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
         LOCAL_START_DATE=$(date -d "$THE_END_DATE +1 days" +%Y-%m-01)
         LOCAL_START_MONTH=$(date -d $LOCAL_START_DATE +%Y-%m)
     done
     
     VAR="'$LOCAL_START_DATE' and '$END_DATE'"
-    sed -i -e "s/\(date_key between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
-    sed -i -e "s/\(transaction_date_key between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
+    sed -i -e "s/\(between \).*/\1$VAR/" ${APP_HOME}/${SQL_FILE}
 fi
